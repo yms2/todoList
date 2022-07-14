8807d5bf290c5414399306cb285a7a23ec77b223
Yu Min Seung <90673332+yms2@users.noreply.github.com>
2022-07-14 17:53:43 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './component/TodoTemplate';
+import TodoHead from './component/TodoHead';
+import TodoList from './component/TodoList';
+import TodoCreate from './component/TodoCreate';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+      <TodoHead/>
+      <TodoList/>
+      <TodoCreate/>
+      </TodoTemplate>
+    </>
+  );
+}
+
+export default App;
